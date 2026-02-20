8bf75e698fb9916a2a300bf6669e90afb7595ce0
BBBiru <aditianugraha625@gmail.com>
2026-02-20 13:35:55 +0700

    mulaipraktikum4

---
+++ praktikum4/praktikum4.py
@@ -0,0 +1,11 @@
+class mobil:
+    def __init__(self, hp, cyl):
+        self.hp = hp
+        self.cyl = cyl
+    
+    def cek_hp():
+        hp = self.hp
+        print(hp)
+
+ModelX = mobil(100, 4)
+ModelX.cek_hp
